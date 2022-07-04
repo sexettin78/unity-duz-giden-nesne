2e949f7fa01e0e6ac9924ac2ca59f7b98151180e
Sexettin <72075007+sexettin78@users.noreply.github.com>
2022-07-04 04:35:18 +0300

    Add files via upload

---
+++ duzgidenaraba.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class duzgidenaraba : MonoBehaviour
+{
+    
+    public float hiz = 0.1f;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+      
+        var pos = transform.position;
+        pos.z += hiz;
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene(8);
+        }
+    }
+}
